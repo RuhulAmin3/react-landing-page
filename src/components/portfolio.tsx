950c3996e6d7b03362dbcbea26e4cefbd837b157
RuhulAmin3 <dev.ruhulamin3@gmail.com>
2023-10-30 22:27:05 +0600

    portfolio section added

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -3,31 +3,47 @@ import Button from "./ui/Button";
 import { BiRightArrow } from "react-icons/bi";
 const Portfolio = () => {
   return (
-    <section className="portfolio">
+    <>
+      <section className="portfolio">
+        <div className="portfolio__titlebar">
+          <p className="portfolio__titlebar-text">Project Gallery</p>
+          <p className="portfolio__titlebar-desc">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis
+            quos, voluptatem quidem suscipit nesciunt fuga deleniti eligendi
+            autem? Ipsa, atque ducimus voluptatum inventore odit molestias
+            soluta ut. Optio, maxime repudiandae!
+          </p>
+        </div>
+      </section>
       <div className="container portfolio__container">
-        <div className="container-full portfolio__bg">
-          <div className="portfolio__titlebar">
-            <p className="portfolio__titlebar-text">Project Gallery</p>
-            <p className="portfolio__titlebar-desc">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Veritatis quos, voluptatem quidem suscipit nesciunt fuga deleniti
-              eligendi autem? Ipsa, atque ducimus voluptatum inventore odit
-              molestias soluta ut. Optio, maxime repudiandae!
-            </p>
-          </div>
-        </div>
         <div className="portfolio__gallary">
-          <img src={portfolioImg} alt="portfolio-image" />
-          <img src={portfolioImg} alt="portfolio-image" />
-          <img src={portfolioImg} alt="portfolio-image" />
-          <img src={portfolioImg} alt="portfolio-image" />
+          <img
+            className="portfolio__img"
+            src={portfolioImg}
+            alt="portfolio-image"
+          />
+          <img
+            className="portfolio__img"
+            src={portfolioImg}
+            alt="portfolio-image"
+          />
+          <img
+            className="portfolio__img"
+            src={portfolioImg}
+            alt="portfolio-image"
+          />
+          <img
+            className="portfolio__img"
+            src={portfolioImg}
+            alt="portfolio-image"
+          />
         </div>
-
+        <div className="portfolio__overlay"></div>
         <Button className="portfolio__btn">
           View More <BiRightArrow />{" "}
         </Button>
       </div>
-    </section>
+    </>
   );
 };
 
