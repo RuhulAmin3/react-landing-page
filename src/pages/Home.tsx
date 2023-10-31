0912480aa34363b04db891819bf63b18068b2fd3
RuhulAmin3 <dev.ruhulamin3@gmail.com>
2023-10-31 20:55:18 +0600

    tablet responsive almost done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,7 +12,7 @@ import Footer from "../components/Footer";
 const Home = () => {
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
       <HeroSection />
       <AboutUs />
       <Services />
